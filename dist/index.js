7620225a80ae480d85726bcb51119888cdb171ab
Gabriel Monutti <gabimonutti1@gmail.com>
2023-01-06 17:14:48 -0300

    Server config done, Cors & Helmet basic config, and RootRouter ready

--- dist/index.js
+++ dist/index.js
@@ -3,25 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-// Configuration the .env file
+const server_1 = __importDefault(require("./src/server"));
+const logger_1 = require("./src/utils/logger");
+// * Configuration the .env file
 dotenv_1.default.config();
-// Create Express APP
-const app = (0, express_1.default)();
 const port = process.env.PORT || 8000;
-// Define the first Route of APP
-app.get("/", (req, res) => {
-    // Send Hello World
-    res.send("Welcome to API:Restful Express + Nodemon + Jest + TS + Swagger + Mongoose");
+// * Execute SERVER
+server_1.default.listen(port, () => {
+    (0, logger_1.LogSuccess)(`[SERVER ON]: Running in http://localhost:${port}/api`);
 });
-// Define the first Route of APP
-app.get("/hello", (req, res) => {
-    // Send Hello World
-    res.send("Welcome to GET Route: Hello!");
-});
-// Execute APP and Listen Requests to PORT
-app.listen(port, () => {
-    console.log(`EXPRESS SERVER: Running at http://localhost:${port}`);
+// * Control SERVER ERROR
+server_1.default.on("error", (error) => {
+    (0, logger_1.LogError)(`[SERVER ERROR]: ${error}`);
 });
 //# sourceMappingURL=index.js.map
